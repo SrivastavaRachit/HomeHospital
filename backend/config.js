778768cc4e2e95528569e4e1d43e3da9bb25877d
m-triple-m <triplem656@gmail.com>
2022-03-13 20:05:00 +0530

    ..

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,6 @@
-const api_config={
-    db_url:
-    "mongodb+srv://Rachit:<R@chit6518619>@cluster0.yfa2c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-
+const api_config = {
+  db_url:
+    "mongodb+srv://Rachit:rachit6518619@cluster0.yfa2c.mongodb.net/homehospital?retryWrites=true&w=majority",
 };
 
 module.exports = api_config;
--- frontend/src/components/main/signup.js
+++ frontend/src/components/main/signup.js
@@ -1,29 +1,34 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import app_config from '../../config';
-import {Formik} from 'formik';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import app_config from "../../config";
+import { Formik } from "formik";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -31,110 +36,116 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function SignUp() {
-
   const userForm = {
-    username: '',
-    email: '',
-    password: ''
-  }
+    username: "",
+    email: "",
+    password: "",
+  };
 
   const url = app_config.api_url;
+
+  
   const signupSubmit = (formdata) => {
-    fetch(url+'/user/add', {
-      method : 'POST',
-      body : JSON.stringify(formdata),
-      headers : {'ContentType': 'application/json'}
+    fetch(url + "/user/add", {
+      method: "POST",
+      body: JSON.stringify(formdata),
+      headers: { ContentType: "application/json" },
     })
-    .then((res) => res.json()).then(data => {
-      console.log(data);
-    })
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
 
   return (
-    
-    
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
           <Formik initialValues={userForm} onSubmit={signupSubmit}>
-      {({values, handleChange, handleSubmit}) => (
-        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              autoComplete="given-name"
-              required
-              fullWidth
-              id="username"
-              value={values.username}
-              onChange={handleChange}
-              label="Username"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              fullWidth
-              id="email"
-              label="email"
-              value={values.email}
-              onChange={handleChange}
-              autoComplete="email"
-            />
-          </Grid>
+            {({ values, handleChange, handleSubmit }) => (
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 3 }}
+              >
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      autoComplete="given-name"
+                      required
+                      fullWidth
+                      id="username"
+                      value={values.username}
+                      onChange={handleChange}
+                      label="Username"
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="email"
+                      label="email"
+                      value={values.email}
+                      onChange={handleChange}
+                      autoComplete="email"
+                    />
+                  </Grid>
 
-          <Grid item xs={12}>
-            <TextField
-              required
-              fullWidth
-              id="password"
-              label="Password"
-              value={values.password}
-              onChange={handleChange}
-              type="password"
-              autoComplete="new-password"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FormControlLabel
-              control={<Checkbox value="allowExtraEmails" color="primary" />}
-              label="I want to receive inspiration, marketing promotions and updates via email."
-            />
-          </Grid>
-        </Grid>
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-        >
-          Sign Up
-        </Button>
-        <Grid container justifyContent="flex-end">
-          <Grid item>
-            <Link href="#" variant="body2">
-              Already have an account? Sign in  
-            </Link>
-          </Grid>
-        </Grid>
-      </Box>
-      )}
-      </Formik>
-          
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="password"
+                      label="Password"
+                      value={values.password}
+                      onChange={handleChange}
+                      type="password"
+                      autoComplete="new-password"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormControlLabel
+                      control={
+                        <Checkbox value="allowExtraEmails" color="primary" />
+                      }
+                      label="I want to receive inspiration, marketing promotions and updates via email."
+                    />
+                  </Grid>
+                </Grid>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign Up
+                </Button>
+                <Grid container justifyContent="flex-end">
+                  <Grid item>
+                    <Link href="#" variant="body2">
+                      Already have an account? Sign in
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            )}
+          </Formik>
         </Box>
         <Copyright sx={{ mt: 5 }} />
       </Container>
