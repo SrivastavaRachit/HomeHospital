8e7d010759c11a448f2b4373d31da5e5d004d0e1
m-triple-m <triplem656@gmail.com>
2022-04-10 18:18:55 +0530

    ..

--- frontend/src/components/admin/index.js
+++ frontend/src/components/admin/index.js
@@ -1,13 +1,26 @@
+import { AccountCircle, Dashboard } from "@mui/icons-material";
 import { Outlet } from "react-router-dom";
-import Footer from "./footer";
-import Header from "./Header";
+import Sidebar from "../SideBar";
 
 const Admin = () => {
+  const sideOptions = [
+    {
+      name: "Dashboard",
+      icon: <Dashboard />,
+      link: "/admin/dashboard",
+    },
+    {
+      name: "Profile",
+      icon: <AccountCircle />,
+      link: "/admin/profile",
+    },
+  ];
+
   return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      <Sidebar sideOptions={sideOptions}>
+        <Outlet />
+      </Sidebar>
     </>
   );
 };
